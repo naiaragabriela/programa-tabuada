500b0a2e7d91f25947e6ac9f0b1a07e0e28dc864
naiaragabriela <naigabi31@gmail.com>
2023-03-05 20:48:36 -0300

    escrita da tabuada utilizando função

---
+++ src/ProgramaTabuada/ProgramaTabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿int numero, resultado = 0;
+int NumeroTabuada()
+{
+    Console.Write("Digite um número inteiro de 1 a 9, que você gostaria de saber a tabuada, e depois digite enter: ");
+   
+    return int.Parse(Console.ReadLine());
+
+}
+
+
+void ValorTabuada()
+{
+    numero = NumeroTabuada();
+    Console.WriteLine("A tabuada desejada é: ");
+
+    for (int i = 0; i <= 10; i++)
+    {
+        resultado = i * numero;
+
+        Console.WriteLine($"{i} * {numero} = {resultado}");
+    }
+}
+
+
+ValorTabuada();
